51523c01e2e016a8df09fbcbdbec1564e5d688a8
marko177 <marco.sarmiento177@gmail.com>
2022-06-26 14:44:15 -0400

    Prueba en clase, ejemplo de programa de while loop

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Este es un cambio para probar en github pero ahora con visual stuido code")
+mes = 0
+
+while mes > 12 or mes < 1:
+    mes = int(input("Introduzca el mes del año entre 1 y 12: "))
+    if mes > 12 or mes < 1:
+        print("Mes introdcido es incorrecto. Inténtelo de nuevo.")
+    
+print(f"El mes {mes} es valido.")
+
+# Prueba en clase Lunes 27 de junio 2022 a las 9:49am
