67b7ac382becd4ace1427fbbaf545e9dff2f9071
marko177 <marco.sarmiento177@gmail.com>
2022-06-27 13:59:12 -0400

    Prueba de funciones martes 904

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-mes = 0
+def area_circulo(r):
+    """Función que recibe el radio del circulo y calcula su area."""
+    area = 3.1415*r**2
+    return area
 
-while mes > 12 or mes < 1:
-    mes = int(input("Introduzca el mes del año entre 1 y 12: "))
-    if mes > 12 or mes < 1:
-        print("Mes introdcido es incorrecto. Inténtelo de nuevo.")
-    
-print(f"El mes {mes} es valido.")
 
-# Prueba en clase Lunes 27 de junio 2022 a las 9:49am
+diametro = 10
+area = area_circulo(diametro/2)
+print(f"Área del círculo de diámetro {diametro} es {area}.")
+
+#Prueba de funciones martes 904
