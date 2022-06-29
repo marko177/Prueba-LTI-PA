38df8418bb850c8f7f287d1217c23a424ab49e1b
marko177 <marco.sarmiento177@gmail.com>
2022-06-29 14:55:48 -0400

    Problema 1 POO con __init__ 30/06/22

--- test.py
+++ test.py
@@ -1,41 +1,19 @@
-class Punto:
+class Cuadrado:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def imprimir(self):
-        print("Cordenadas del punto")
-        print(f"({self.x},{self.y})")
+    def __init__(self):
 
-    def imprimir_cuadrante(self):
+        self.lado = float(input("Ingrese la longitud de un lado del cuadrado:\n"))
 
-        if self.x > 0 and self.y > 0:
-            print("Primer cuadrante.")
+    def imprimir_perimietro(self):
 
-        elif self.x < 0 and self.y > 0:
-            print("Segundo cuadrante")
-        
-        elif self.x < 0 and self.y < 0:
-            print("Tercer cuadrante")
-        
-        elif self.x > 0 and self.y < 0:
-            print("Cuarto cuadrante")
+        print(f"El perimetro del cuadrado es {self.lado * 4}")
 
-punto1 = Punto(2, 3)
-punto1.imprimir()
-punto1.imprimir_cuadrante()
+    def imprimir_area(self):
 
-punto2 = Punto(-3, 1)
-punto2.imprimir()
-punto2.imprimir_cuadrante()
+        print(f"El area del cuadrado es: {self.lado * self.lado}")
 
-punto3 = Punto(-1, -2)
-punto3.imprimir()
-punto3.imprimir_cuadrante()
+cuadrado1 = Cuadrado()
+cuadrado1.imprimir_perimietro()
+cuadrado1.imprimir_area()
 
-punto4 = Punto(10, -2)
-punto4.imprimir()
-punto4.imprimir_cuadrante()
-
-# Prueba 2 de POO con __init__ 30/06/22
+# Problema 1 POO con __init__ 30/06/22
