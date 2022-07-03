b16b98f696cde0d037c40ef4278963463c100726
marko177 <marco.sarmiento177@gmail.com>
2022-07-03 14:08:30 -0400

    Prueba 3 Listas 04/07/2022

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-meses = ["enero", "febrero", "marzo", "abril"]
+lista = ["ana", 7, 9]
 
-print(meses[0])
-print(meses[3])
+print(f"Nombre del alumno: {lista[0]}")
+promedio = (lista[1] + lista[2]) / 2
+print(f"Promedio de sus dos notas: {promedio}")
 
-#Prueba 2 Listas 04/07/2022
+#Prueba 3 Listas 04/07/2022
