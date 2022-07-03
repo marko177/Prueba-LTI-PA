1bc5c4a3be5e9a2abc07ea2dc833c14aa1e07575
marko177 <marco.sarmiento177@gmail.com>
2022-07-03 14:01:28 -0400

    Prueba 2 Listas 04/07/2022

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-lista = [10 , 7, 3, 7, 2]
+meses = ["enero", "febrero", "marzo", "abril"]
 
-suma = 0
-x = 0
+print(meses[0])
+print(meses[3])
 
-while x < len(lista):
-
-    suma += lista[x]
-    x += 1
-
-print(f"Los elementos de la lista son:\n{lista}")
-print(f"La suma de todos sus elementos es: {suma}")
-
-#Prueba 1 Listas 04/07/2022  
+#Prueba 2 Listas 04/07/2022
