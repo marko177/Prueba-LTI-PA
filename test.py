67c5ea2ded3df647c499b69430831df9f08c51bd
marko177 <marco.sarmiento177@gmail.com>
2022-07-03 14:57:11 -0400

    Corrección Problema 2 Listas 04/07/2022

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ lista = [1, 3, 7, 18, 31]
 mayor_7 = []
 
 for i in lista:
-    if i > 7:
+    if i >= 7:
         mayor_7.append(i)
 
-print(f"En la lista {lista}, los siguientes son mayores a 7: {mayor_7}")
+print(f"En la lista {lista}, los siguientes son mayores a 7: {mayor_7}.")
 
-#Problema 2 Listas 04/07/2022
+#Corrección Problema 2 Listas 04/07/2022
