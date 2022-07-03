5ed17780face00ab2f0e90a0bb44ecbab97a89c5
marko177 <marco.sarmiento177@gmail.com>
2022-07-03 15:08:38 -0400

    Corrección Problema 3 Listas 04/07/2022

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 lista = ["marco", "leslie", "michelle", "ana", "luz"]
 
-mayor_5 = []
+mayor_5 = 0
 
 for i in lista:
     if len(i) >= 5:
-        mayor_5.append(i)
+        mayor_5 += 1
 
-print(f"En la lista {lista}, los siguientes nombres tienen 5 o mas caracteres: {mayor_5}")
+print(f"En la lista {lista}, hay {mayor_5} nombres que tienen 5 o mas caracteres")
 
-#Problema 3 Listas 04/07/2022
+#Corrección Problema 3 Listas 04/07/2022
