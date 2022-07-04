e595808e8ca05729329100790824479bdd650a53
marko177 <marco.sarmiento177@gmail.com>
2022-07-04 13:01:06 -0400

    Prueba 1 Listas Input 05/07/2022

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 lista = []
 
-for x in range(5):
-    
-    valor = int(input("Ingrese un valor entero:\n"))
-    lista.append(valor)
+valor = int(input("Ingrese un valor entero (0 para terminar):\n"))
 
+while valor != 0:
+
+    lista.append(valor)
+    valor = int(input("Ingrese un valor entero (0 para terminar):\n"))
+print(f"Tamaño de la lista: {len(lista)}")
 print(lista)
 
-#Prueba 1 Listas Append 04/07/2022
+#Prueba 1 Listas Input 05/07/2022
