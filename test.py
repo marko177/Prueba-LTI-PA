ba4008999b73eed5c9be799ea870230a49d1a297
marko177 <marco.sarmiento177@gmail.com>
2022-07-05 15:27:06 -0400

    Prueba 1 Leer archivos por linea 06/07/2022

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 archi1 = open("datos.txt", "r")
-contenido = archi1.read()
-print(contenido)
+linea = archi1.readline()
+
+while linea != "":
+    print(linea, end = "")
+    linea = archi1.readline()
+
 archi1.close()
 
-# Prueba 1 Leer archivos desde python 06/07/2022
+# Prueba 1 Leer archivos por linea desde python 06/07/2022
