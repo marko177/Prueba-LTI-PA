662f5e299f220ab4e9837d31dd0223f2bdd79457
marko177 <marco.sarmiento177@gmail.com>
2022-07-05 14:10:03 -0400

    Problema 3 Ordenado de listas 06/07/2022

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
-sueldos = []
+lista_num = []
 
-num_empleados = int(input("Ingrese el numero de empleados:\n"))
-
-for x in range(num_empleados):
+for x in range(5):
     
-    sueldo = int(input("Ingrese el sueldo del empleado:\n"))
-    sueldos.append(sueldo)
+    num = int(input("Ingrese un numero entero:\n"))
+    lista_num.append(num)
 
-print(f"Lista de sueldos sin ordenar:\n{sueldos}")
+print(f"Lista de numeros sin ordenar:\n{lista_num}")
 
-for k in range(len(sueldos) - 1):
+for k in range(len(lista_num) - 1):
 
-    for x in range((len(sueldos) - 1) - k):
+    for x in range((len(lista_num) - 1)):
 
-        if sueldos[x] > sueldos[x + 1]:
+        if lista_num[x] > lista_num[x + 1]:
             
-            aux = sueldos[x]
-            sueldos[x] = sueldos[x + 1]
-            sueldos[x + 1] = aux
+            aux = lista_num[x]
+            lista_num[x] = lista_num[x + 1]
+            lista_num[x + 1] = aux
 
 
-print(f"Lista de sueldos ordenados:\n{sueldos}")
+print(f"Lista de numeros ordenados menor a mayor:\n{lista_num}")
 
-# Problema 2 Ordenado de listas 06/07/2022
+for k in range(len(lista_num) - 1):
 
+    for x in range((len(lista_num) - 1)):
+
+        if lista_num[x] < lista_num[x + 1]:
+            
+            aux = lista_num[x]
+            lista_num[x] = lista_num[x + 1]
+            lista_num[x + 1] = aux
+
+
+print(f"Lista de numeros ordenados mayor a menor:\n{lista_num}")
+
+# Problema 3 Ordenado de listas 06/07/2022
